c52a2edbfdd28676efb5d46fbce02db5d5da2a97
Napoleon-Christos Oikonomou <napoleonoikon@gmail.com>
2022-04-25 18:33:02 +0300

    convert to ts

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
-const average = (input, options = {}) => {
+type Options = {
+	excludeZeroes?: boolean;
+	excludeFalsies?: boolean;
+	get?: (v: unknown) => number;
+};
+
+const average = (input: unknown[], options: Options = {}) => {
 	options = {
 		excludeZeroes: false,
 		excludeFalsies: false,
-		get: (v) => v,
+		get: (v) => v as number,
 		...options,
 	};
 	if (!Array.isArray(input)) throw new TypeError("`input` must be an array");
@@ -11,8 +17,8 @@ const average = (input, options = {}) => {
 	const count = options.excludeFalsies || options.excludeZeroes ? input.filter(options.get).length : input.length;
 	if (!count) return Number.NaN;
 
-	const sum = input.reduce((accumulator, currentValue) => {
-		const term = options.get(currentValue);
+	const sum = input.reduce((accumulator: number, currentValue) => {
+		const term = options.get!(currentValue);
 		if (options.excludeFalsies && !term) return accumulator;
 		return accumulator + term;
 	}, 0);
--- tests/main.test.ts
+++ tests/main.test.ts
@@ -9,7 +9,7 @@ test("main", (t) => {
 	t.is(average([0, 1, 2, Number.NaN, undefined]), Number.NaN);
 	t.is(average([0, 1, 2, Number.NaN, undefined], { excludeZeroes: true }), Number.NaN);
 	t.is(average([0, 1, 2, Number.NaN, undefined], { excludeFalsies: true }), 1.5);
-	t.is(average([{ name: "iamnapo", age: 28 }, { age: 28 }, { name: "Alice", age: 25 }], { get: (v) => v.age }), 27);
+	t.is(average([{ name: "iamnapo", age: 28 }, { age: 28 }, { name: "Alice", age: 25 }], { get: (v) => (v as { age: number }).age }), 27);
 	t.is(average([]), Number.NaN);
 });
 
